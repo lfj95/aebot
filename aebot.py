2a356082991e8767427d6cae2a953143c6d92977
lfj95 <55749277+lfj95@users.noreply.github.com>
2021-09-28 06:38:15 +0100

    Add files via upload

--- aebot.py
+++ aebot.py
@@ -15,7 +15,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def initDB():
   global cache 
-  cache = sqlite3.connect(home + "/.cbot_cache") 
+  cache = sqlite3.connect(home + "/.aebot_cache")
   cache.execute("""
                    CREATE TABLE IF NOT EXISTS questions 
                    (id INTEGER PRIMARY KEY,
@@ -44,7 +44,6 @@ def fetchQ():
         question = question + " " + sys.argv[a]
         #sys.argv() is an array for command line arguments in Python.Command line arguments are those values that are passed during calling of program along with the calling statement. Thus, the first element of the array sys.argv() is the name of the program itself. The values are also retrieved like Python array.
     question = question.strip() #The strip() method returns a copy of the string by removing both the leading and the trailing characters (based on the string argument passed).
-    print(question)
     return question
 
 def parseOptions(question):
@@ -69,7 +68,8 @@ def parseOptions(question):
 def insertQ(question_text,answer_text):
     answer = cache.execute("DELETE FROM questions WHERE question = ?",(question_text,))
     answer = cache.execute("INSERT INTO questions (question,answer) VALUES (?,?)", (question_text,answer_text))
-
+# 该例程执行一个 SQL 语句。该 SQL 语句可以被参数化（即使用占位符代替 SQL 文本）。sqlite3 模块支持两种类型的占位符：问号和命名占位符（命名样式）。
+#例如：cursor.execute("insert into people values (?, ?)", (who, age))
 
 global question
 question = ""
@@ -84,15 +84,15 @@ initDB()
 
 
 #check if there's an aswer in our cache, then execute a GPT3 request as needed
-cache_answer = checkQ(question)   
-
+cache_answer = checkQ(question)
 if not(cache_answer):
     if (question_mode == "normal"):
         prompt="I am an action effect prediction bot.\n\nQ: What is the effect of hold a cup upside down?\nA: Spilling the liquid inside.\n\nQ: What is the effect of peel carrots?\nA: The peels will be separated from carrots.\n\nQ: What is the effect of crack an egg?\nA: The entire contents of the egg will be in the bowl, with the yolk unbroken, and that the two halves of the shell are held in the cook’s fingers.\n\nQ: What is the effect of heat water?\nA: The water will begin to boil, which is evinced by bubbles forming and rising to the surface.\n\nQ: What is the effect of shake can?\nA: The liquid inside will be shaken up and will form into a foamy head.\n\nQ: What is the effect of cut cucumber?\nA: The cucumber will be cut into two pieces.\n"
+        temp_question = question
         if not("?" in question):
             temp_question = question + "?"  # GPT produces better results if there's a question mark.
                                             # using a temp variable so the ? doesn't get cached
-        prompt = prompt + "Q: " + temp_question + "\n" 
+        prompt = prompt + "Q: " + temp_question + "\n"
         start_sequence = "\nA:"
         restart_sequence = "\n\nQ: "
         response = openai.Completion.create(
@@ -118,10 +118,13 @@ if not(cache_answer):
         )
 
     jsonToPython = json.loads(response.last_response.body)
-    result = jsonToPython['choices'][0]['text']
-    insertQ(question, result) 
+    result = jsonToPython['choices'][0]['text'].strip("A: ")
+    print("This is a new action effect prediction")
+    print(result)
+    insertQ(question, result)
 
 else:
-    result = cache_answer 
-
+    result = cache_answer
+    print("This prediction comes from cache_answer")
+    print(cache_answer)
 closeDB()
